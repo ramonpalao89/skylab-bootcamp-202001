a8833aaf6f56818630ff7afbbe83f9922c207086
ramon <ramon.palao@gmail.com>
2020-02-19 20:37:10 +0100

    restructure files on public and util folders to execute express

--- staff/ramon-palao/web-server/index.js
+++ staff/ramon-palao/web-server/index.js
@@ -1,32 +1,48 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
-const logger = require('./logger')
+// const http = require('http')
+// const fs = require('fs')
+// const url = require('url')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
 
 logger.info('starting server')
-
-const server = http.createServer((req, res) => {
-
+const myLogger = function (req, res, next) {
     logger.info(`request from IP: ${req.socket.remoteAddress}`)
+    next()
+  }
+  
+app.use(myLogger)
 
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname;
-    if (filename === './') filename = 'index.html'
+app.use(express.static('public'))
 
-    // let path = req.url
-    // if (path === './') path += 'index.html'
-    // path = `.${path}`
+app.use(express.static('utils'))
+
+app.listen(8080)
+
+// logger.info('starting server')
+
+// const server = http.createServer((req, res) => {
+
+//     logger.info(`request from IP: ${req.socket.remoteAddress}`)
+
+//     let q = url.parse(req.url, true);
+//     let filename = "." + q.pathname;
+//     if (filename === './') filename = 'index.html'
+
+//     // let path = req.url
+//     // if (path === './') path += 'index.html'
+//     // path = `.${path}`
     
-    fs.readFile(filename, function (error, html) {
-        res.setHeader("Content-Type", "text/html")
+//     fs.readFile(filename, function (error, html) {
+//         res.setHeader("Content-Type", "text/html")
 
-        if (error) {
-            logger.error(error)
-            res.writeHead(404)
-            return res.end("404 Not Found");
-        } 
-        res.write(html);
-        res.end();
-    })
-})
-server.listen(8080)
+//         if (error) {
+//             logger.error(error)
+//             res.writeHead(404)
+//             return res.end("404 Not Found");
+//         } 
+//         res.write(html);
+//         res.end();
+//     })
+// })
+// server.listen(8080)
