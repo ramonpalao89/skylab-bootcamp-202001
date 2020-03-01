1ccd9814c826bff07608c5a32dbf2be4a597c5f2
ramon <ramon.palao@gmail.com>
2020-03-01 16:46:50 +0100

    add subscribe events errors to logic

--- staff/ramon-palao/events-api/logic/subscribe-events.js
+++ staff/ramon-palao/events-api/logic/subscribe-events.js
@@ -1,26 +1,22 @@
 const { validate } = require('../utils')
 const { database, database: { ObjectId } } = require('../data')
-const { NotFoundError, NotAllowedError } = require('../errors')
+const { NotAllowedError } = require('../errors')
 
 module.exports = (idUser, idEvent) => {
 
     validate.string(idUser, 'idUser')
     validate.string(idEvent, 'idEvent')
 
-    //const _id = ObjectId(idUser)
-
     const events = database.collection('events')
     const users = database.collection('users')
 
-    return users.updateOne({_id: ObjectId(idUser)}, {$push:{subscribedEvents: ObjectId(idEvent)}} )
-        .then(() => events.updateOne({_id: ObjectId(idEvent)}, {$push:{subscribers: ObjectId(idUser)}}))
-        .then(() => { })
+    return users.findOne({ _id: ObjectId(idUser) ,  subscribedEvents: ObjectId(idEvent) })
+        .then(user => {
 
-    // return events.find({ publisher: _id }).toArray()
-    //     .then(event => {
+            if (user) throw new NotAllowedError(`User with id ${idUser} is already subscribed to this event`)
 
-    //         if (!event.length) throw new NotFoundError(`User with id ${id} has not published any event`)
-
-    //         return event
-    //     })
+            return users.updateOne({ _id: ObjectId(idUser) }, { $push: { subscribedEvents: ObjectId(idEvent) } })
+            .then(() => events.updateOne({ _id: ObjectId(idEvent) }, { $push: { subscribers: ObjectId(idUser) } }))
+            .then(() => { })
+        })
 }
--- staff/ramon-palao/events-api/routes/subscribe-events.js
+++ staff/ramon-palao/events-api/routes/subscribe-events.js
@@ -1,5 +1,5 @@
 const { subscribeEvents } = require('../logic')
-const { ContentError } = require('../errors')
+const { ContentError, NotAllowedError } = require('../errors')
 
 module.exports = (req, res) => {
     const { payload: { sub: id }, body: { idEvent } } = req
@@ -9,7 +9,7 @@ module.exports = (req, res) => {
             .then(() => res.status(201).end()
             )
             .catch(error => {
-                let status = 400
+                let status = 406
                 const { message } = error
                 res
                     .status(status)
@@ -21,7 +21,7 @@ module.exports = (req, res) => {
     } catch (error) {
         let status = 400
 
-        if (error instanceof TypeError || error instanceof ContentError)
+        if (error instanceof TypeError || error instanceof ContentError || error instanceof NotAllowedError)
             status = 406 // not acceptable
 
         const { message } = error
